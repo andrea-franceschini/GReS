5f2ce169abfb68b2935e1af5504f39853a7d04af
Dzale001 <dzale001@gmail.com>
2024-09-19 09:04:08 +0200

    E stata modificata la classe Elastic.m e aggiunti i file PorousMedia2.dat e materialsList2.dat. La classe DKPlastic e stata scritta con il solo scopo di poter poi differenziare da caso elastico puro a caso elastico con la legge di drucker-prager

--- Code/materials/Elastic.m
+++ Code/materials/Elastic.m
@@ -77,4 +77,58 @@ classdef Elastic < handle
                   q = sqrt(0.5*((sigmaIn(i, 1)-sigmaIn(i, 2))^2+(sigmaIn(i, 1)- ...
                       sigmaIn(i, 3))^2+(sigmaIn(i, 2)-sigmaIn(i, 3))^2)+ ...
                       sigmaIn(i, 4)^2+sigmaIn(i, 5)^2+sigmaIn(i, 6)^2); %calcolo di q
-                  %k = 0; % inizialmente: dovr
+                  %k = 0; % inizialmente: dovrebbe essere obj.h*εp ma la
+                  %deformazione plastica e zero
+                  p = sum(sigmaIn(i, 1:3)); %calcolo di p
+                  force = q/1.732 + obj.alpha*p-obj.epsilon*(obj.co);
+                  
+                  while force > 1e-10
+                      %fprintf("force is positive \n");
+                      sigma = sigmaIn;
+                      G = obj.E/(2*(1+obj.nu));
+                      K = obj.E/(3*(1+obj.nu));
+                      %s = sigmaIn(i, 1:3)- p/3;
+                      n = (1.5/q).*(sigma(i, 1:6)- p/3);
+                      %fprintf("%.18f \n", force);
+                      % Δλ
+                      lambdac = max(0, force/(G+obj.alpha*obj.beta*K+(obj.epsilon^2)*obj.h));% fprintf("%.16f | lambdac \n", lambdac);
+                      p = p-lambdac*K*obj.beta;%fprintf("%.16f | p \n", p);
+                      if q-lambdac*1.732*G < 0
+                          %fprintf("q = 0 \n");
+                          %force = obj.alpha*p - obj.epsilon*(obj.co);fprintf("%.22f \n", force); %no shear force 
+                          sigma(i, 1:6) = (p-lambdac*K*obj.beta).*ones(1,6);
+                      else
+                          q = q-lambdac*1.732*G;%fprintf("%.16f | q \n", q);
+                          force = q/1.732 + obj.alpha*p-obj.epsilon*(obj.co);% fprintf("%.22f \n", force);
+                          sigma(i, 1:6) = sigma(i, 1:6) - lambdac.*((2*G/1.732).*n+K*obj.beta);
+                      end
+                      %fprintf("%.16f | sigma \n", sigma);
+                  end
+               end
+        end
+  end
+
+  methods (Access = private)
+    % Assigning material parameters (check also the Materials class)
+    % to object properties
+    function readMaterialParameters(obj, fID, matFileName)
+      tmpVec = readDataInLine(fID, matFileName, 6);
+      %
+      obj.E = tmpVec(1);
+      obj.nu = tmpVec(2);
+      %
+      % Compute the M factor
+      obj.M = obj.nu/(1-obj.nu);
+      %
+      % Compute vertical compressibility
+      obj.cM = (1+obj.nu)*(1-2*obj.nu)/(obj.E*(1-obj.nu));
+      obj.psi = tmpVec(3);
+      obj.phi = tmpVec(4);
+      obj.co = tmpVec(5); %coesione
+      obj.h = tmpVec(6); %hardening parameter
+      obj.alpha = (3*tan(obj.phi))/(sqrt(9+12*tan(obj.phi)^2)); 
+      obj.beta = (3*tan(obj.psi))/(sqrt(9+12*tan(obj.psi)^2));
+      obj.epsilon = 3/(sqrt(9+12*tan(obj.phi)^2));
+    end
+  end
+end
