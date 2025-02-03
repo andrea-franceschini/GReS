c4293e1d9b9f64d561772d3304d1debe72bea098
danielemoretto44 <danielemoretto44@gmail.com>
2025-02-03 08:48:48 +0100

    Removed deprecated files before merging into main branch

--- Code/simparam/Test_simulation_parameters.m
+++
@@ -1,7 +0,0 @@
-close all
-clear
-addpath('../modeltype');
-% model = ModelType("VariabSatFlow_FVTPFA");
-model = ModelType("SinglePhaseFlow_FVTPFA");
-fileName = "SimParamSPF.dat";
-simParam = SimulationParameters(model,fileName)
