26c19cd605ea2f15f3fa0030dc616c581b5484be
castro-eng <castro.eng@gmail.com>
2025-09-05 12:06:09 +0200

    Finish the clean up.

--- Code/unittest/runUnitTests.m
+++ Code/unittest/runUnitTests.m
@@ -4,4 +4,4 @@ runtests(fullfile('Mesh','testMesh.m'));
 runtests(fullfile('DoFManager','testDoFManager.m'));
 % runtests('Material/testMaterialN.m');
 runtests('Simparam/testSimparam.m');
-runtests('GrowningDomain/testGrowning.m');
+% runtests('GrowningDomain/testGrowning.m');
