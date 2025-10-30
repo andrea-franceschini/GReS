73cc45dea9c582746304bfb884085e9da88bce66
Daniele Moretto <daniele.moretto.3@studenti.unipd.it>
2025-10-29 18:10:50 -0700

    Modified gitignore

--- Code/unittest/runUnitTests.m
+++ Code/unittest/runUnitTests.m
@@ -1,5 +1,15 @@
 clear
 clc
-runtests(fullfile('Mesh','testMesh.m'));
-runtests(fullfile('DoFManager','testDoFManager.m'));
-runtests('Simparam/testSimparam.m');
+
+
+testFiles = {fullfile('Mesh','testMesh.m');...
+             fullfile('DoFManager','testDoFManager.m');...
+             fullfile('Simparam','testSimparam.m')};
+
+results = runtests(testFiles);
+
+if any([results.Failed])
+  error("Some unit tests not passed");
+else
+  disp("All test run successfully")
+end
