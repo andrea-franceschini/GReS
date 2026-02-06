e3187f490a2b9b8750547dd8a099242939191ab3
castro-eng <53985140+castro-eng@users.noreply.github.com>
2026-02-06 12:18:41 +0100

    Updating the tutorials (#62)

--- Tutorial/stepBystepSimulation/stepBystep.m
+++ Tutorial/stepBystepSimulation/stepBystep.m
@@ -10,9 +10,11 @@ scriptDir = fileparts(scriptFullPath);
 % Change the current directory to the script's directory
 cd(scriptDir);
 
+gresLog().setVerbosity(2)
+
 %% STEP 1) SINGLE PHYSICS - SINGLE PHASE FLOW
 
-fileName = "01_singlePhysics.xml";
+fileName = "model01/01_singlePhysics.xml";
 
 % Set parameters of the simulation
 simParam = SimulationParameters(fileName);
@@ -47,10 +49,10 @@ Solver.NonLinearLoop();
 domain.outstate.finalize()
 
 
-%% STEP 2) COUPLING WITH MECHANICS
+%% STEP 2) ADDING SUBDOMAINS
 
 
-fileName = "02_moreMaterials.xml";
+fileName = "model02/02_moreMaterials.xml";
 
 % Set parameters of the simulation
 simParam = SimulationParameters(fileName);
@@ -90,8 +92,188 @@ Solver.NonLinearLoop();
 
 domain.outstate.finalize()
 
-%% STEP 3) USING SUBDOMAINS
+%% STEP 3) COUPLING WITH MECHANICS
+
+
+
+fileName = "model03/03_coupledPoromechanics.xml";
+
+% Set parameters of the simulation
+simParam = SimulationParameters(fileName);
+
+% Create an object of the Materials class and read the materials file
+mat = Materials(fileName);
+
+mesh = structuredMesh(15,15,10,[0 100],[0 100],[0 10]);
+
+gaussOrder = 2;
+elems = Elements(mesh,gaussOrder);
+faces = Faces(mesh);
+grid = struct('topology',mesh,'cells',elems,'faces',faces);
+
+% we manually modify the tag for some clay overburden and underburden
+over = mesh.cellCentroid(:,3) < 3;
+under = mesh.cellCentroid(:,3) > 7;
+mesh.cellTag(under) = 2;
+mesh.cellTag(over) = 3;
+
+bound = Boundaries(fileName,grid);
+
+printUtils = OutState(mesh,fileName);
+
+domain = Discretizer('Boundaries',bound,...
+                     'OutState',printUtils,...
+                     'Materials',mat,...
+                     'Grid',grid);
+domain.addPhysicsSolver(fileName);
+
+
+Solver = GeneralSolver(simParam,domain);
+
+Solver.NonLinearLoop();
+
+domain.outstate.finalize();
+
 
 %% STEP 4) ADDING A NON-CONFORMING INTERFACE
+clc
+clear
 
-%% STEP 5) CONTACT MECHANICS AND EMBEDDED FRACTURES
+%%%%%%%%%%% domain 1
+
+fileName = "model04/04_domRight.xml";
+
+
+mesh1 = structuredMesh(15,15,10,[20 100],[0 100],[0 10]);
+elems = Elements(mesh1,2);
+faces = Faces(mesh1);
+grid = struct('topology',mesh1,'cells',elems,'faces',faces);
+
+% we manually modify the tag for some clay overburden and underburden
+over = mesh1.cellCentroid(:,3) < 3;
+under = mesh1.cellCentroid(:,3) > 7;
+mesh1.cellTag(under) = 2;
+mesh1.cellTag(over) = 3;
+
+mat = Materials(fileName);
+bound = Boundaries(fileName,grid);
+
+printUtils = OutState(mesh1,fileName);
+
+domain1 = Discretizer('Boundaries',bound,...
+                     'OutState',printUtils,...
+                     'Materials',mat,...
+                     'Grid',grid);
+domain1.addPhysicsSolver(fileName);
+
+
+%%%%%%%%%%% domain 2
+
+fileName = "model04/04_domLeft.xml";
+
+
+mesh2 = structuredMesh(6,6,4,[0 20],[0 100],[0 10]);
+elems = Elements(mesh2,2);
+faces = Faces(mesh2);
+grid = struct('topology',mesh2,'cells',elems,'faces',faces);
+
+mat = Materials(fileName);
+bound = Boundaries(fileName,grid);
+
+printUtils = OutState(mesh2,fileName);
+
+domain2 = Discretizer('Boundaries',bound,...
+                     'OutState',printUtils,...
+                     'Materials',mat,...
+                     'Grid',grid);
+domain2.addPhysicsSolver(fileName);
+
+domains = [domain1;domain2];
+
+interfaces = buildInterfaces('model04/04_nonConformingInterface.xml',domains);
+
+%%%%
+
+simParam = SimulationParameters('model04/04_nonConformingInterface.xml');
+
+Solver = GeneralSolver(simParam,domains,interfaces);
+
+Solver.NonLinearLoop();
+
+Solver.finalizeOutput();
+
+
+
+%% STEP 5) ADDING A FAULT
+clc
+clear
+
+%%%%%%%%%%% domain 1
+
+fileName = "model05/05_domRight.xml";
+
+mesh1 = structuredMesh(15,15,10,[20 100],[0 100],[0 10]);
+elems = Elements(mesh1,2);
+faces = Faces(mesh1);
+grid = struct('topology',mesh1,'cells',elems,'faces',faces);
+
+% we manually modify the tag for some clay overburden and underburden
+over = mesh1.cellCentroid(:,3) < 3;
+under = mesh1.cellCentroid(:,3) > 7;
+mesh1.cellTag(under) = 2;
+mesh1.cellTag(over) = 3;
+
+mat = Materials(fileName);
+bound = Boundaries(fileName,grid);
+
+printUtils = OutState(mesh1,fileName);
+
+domain1 = Discretizer('Boundaries',bound,...
+                     'OutState',printUtils,...
+                     'Materials',mat,...
+                     'Grid',grid);
+domain1.addPhysicsSolver(fileName);
+
+
+%%%%%%%%%%% domain 2
+
+fileName = "model05/05_domLeft.xml";
+
+
+mesh2 = structuredMesh(6,6,4,[0 20],[0 100],[0 10]);
+elems = Elements(mesh2,2);
+faces = Faces(mesh2);
+grid = struct('topology',mesh2,'cells',elems,'faces',faces);
+
+mat = Materials(fileName);
+bound = Boundaries(fileName,grid);
+
+printUtils = OutState(mesh2,fileName);
+
+domain2 = Discretizer('Boundaries',bound,...
+                     'OutState',printUtils,...
+                     'Materials',mat,...
+                     'Grid',grid);
+domain2.addPhysicsSolver(fileName);
+
+domains = [domain1;domain2];
+
+interfaces = buildInterfaces('model05/05_fault.xml',domains);
+
+% set initial traction
+% apply initial traction to the interface
+tIni = -1.0;
+interfaces{1}.state.traction(1:3:end) = tIni;
+interfaces{1}.state.iniTraction(1:3:end) = tIni;
+interfaces{1}.stateOld.iniTraction(1:3:end) = tIni;
+interfaces{1}.stateOld.traction(1:3:end) = tIni;
+
+%%%%
+
+simParam = SimulationParameters('model05/05_fault.xml');
+
+Solver = GeneralSolver(simParam,domains,interfaces);
+
+Solver.NonLinearLoop();
+
+Solver.finalizeOutput();
