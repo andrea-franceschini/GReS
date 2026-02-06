6c7c955662295d877ddb6fdd70214a4543decf7b
Daniele <danielemoretto44@gmail.com>
2026-02-07 00:15:03 +0100

    Fixed compilation error after moving mex files

--- compileAll.m
+++ compileAll.m
@@ -3,16 +3,16 @@ clear;
 close all;
 
 list = {'Code/read',...
-  'Code/write', ...
+  'Code/output', ...
   'Code/elements',...
   'ThirdPartyLibs/PolyClipper'};
 
 home = pwd;
 for folder = list
-    fprintf('Compiling MEX files in %s\n', folder{1});
-    cd(folder{1});
-    compile
-    fprintf('\n\n')
-    cd(home);
+  fprintf('Compiling MEX files in %s\n', folder{1});
+  cd(folder{1});
+  compile
+  fprintf('\n\n')
+  cd(home);
 end
 clear
