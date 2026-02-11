b22c41747bce84f1db6cf8ea8c296d6b9a7c66d3
Daniele Moretto <127932902+danielemoretto44@users.noreply.github.com>
2026-02-11 11:22:40 +0100

    With updated plot utilities (#70)

--- Utilities/ParaviewPlot/plotFunction.m
+++ Utilities/ParaviewPlot/plotFunction.m
@@ -1,23 +1,42 @@
-function  plotFunction(mesh, foldName, funct, varargin)
+function  plotFunction(mesh, foldName, time, funct, varargin)
 % PLOTFUNCTION Summary of this function goes here
 %   Detailed explanation goes here
-outVTK = VTKOutput(mesh, foldName); % create VTK object
-pointData.name = 'solution';
-pointData.data = funct;
-outVTK.writeVTKFile(0, [], [], [], []);
+
+out = OutState('folderName',foldName,'timeList',time);
+out.prepareOutputFolders();
+
+[~, vtuName, ~] = fileparts(foldName);
+
+toc = out.vtkFile.getDocumentElement;
+toc.setAttribute('type', 'vtkMultiBlockDataSet');
+toc.setAttribute('version', '1.0');
+blocks = out.vtkFile.createElement('vtkMultiBlockDataSet');
+
+
+% inner block with vtu dataset to append to vtm file
+block = out.vtkFile.createElement('Block');
+
+var.name = 'solution';
+var.data = funct;
+% outVTK.writeVTKFile(0, [], [], [], []);
 if isempty(varargin) || strcmpi(varargin{1},'node')
-   if ~isempty(mesh.cells)
-      outVTK.writeVTKFile(0, pointData, [], [], []);
-   else
-      outVTK.writeVTKFile(0, [], [], pointData, []);
-   end
+  if ~isempty(mesh.cells)
+    writeVTKfile(out,block,vtuName,mesh,time,var,[],[],[]);
+  else
+    writeVTKfile(out,block,vtuName,mesh,time,[],[],var,[]);
+  end
 elseif strcmpi(varargin{1},'elem')
-   if ~isempty(mesh.cells)
-      outVTK.writeVTKFile(0, [], pointData, [], []);
-   else
-      outVTK.writeVTKFile(0, [], [], [], pointData);
-   end
+  if ~isempty(mesh.cells)
+    writeVTKfile(out,block,vtuName,mesh,time,[],var,[],[]);
+  else
+    writeVTKfile(out,block,vtuName,mesh,time,[],[],[],var);
+  end
 end
-   outVTK.finalize()
+
+blocks.appendChild(block);
+toc.appendChild(blocks);
+
+out.writeVTMFile();
+
 end
 
