c0f566be1404371b20f4642ecc9cb38a7d025791
Andrea <Andrea@10.51.248.187>
2026-02-19 18:01:57 +0100

    Updating comment

--- upscaling/transform_joint_compliance.m
+++ upscaling/transform_joint_compliance.m
@@ -5,10 +5,11 @@ function S_global = transform_joint_compliance(S_j, R)
     S_j_voigt = zeros(6);
 
     % Mapping from Marcio's code
-    % the sample is aligned with x, thus:
+    % the joint's normal is aligned with x, thus:
     % n -> x
     % t -> xz
     % t -> xy
+    % it will be rotated by T
     S_j_voigt(1,1) = S_j(3,3);  % normal compliance
     S_j_voigt(5,5) = S_j(2,2);  % shear
     S_j_voigt(6,6) = S_j(1,1);  % shear
