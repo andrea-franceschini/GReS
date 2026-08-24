05fa531c38beca383f2727f9254c7a04aed51337
castro-eng <castro.eng@gmail.com>
2026-08-24 10:36:52 +0200

    Update Sedimentation class.

--- Tests/Sedimentation/TidalMarsh/runTidalMarsh.m
+++ Tests/Sedimentation/TidalMarsh/runTidalMarsh.m
@@ -6,12 +6,12 @@ file_Solver = fullfile(input_dir,'solver.xml');
 
 %% ------------------------------ Set up the Domain -----------------------
 % Set the simulation parameters for the non-linear solver.
-simParam = SimulationParameters('Start',0.,'End',1.0e2,...
+simParam = SimulationParameters('Start',0.,'End',3.0e3,...
       'DtInit',1e-0,'DtMin',1e-1,'DtMax',1e-0,'MaxNLIteration',30);
 
 % Create an object of the Materials class and read the materials file
 mat = Materials(file_Mat);
-printUtils = OutState('outputFile','Output/Results2','printTimes',0:1:3000,"vtkFormat","binary");
+printUtils = OutState('outputFile','Output/Results2','printTimes',0:25:3000,"vtkFormat","binary");
 
 % Create object handling construction of Jacobian and rhs of the model
 domain = Discretizer('Materials',mat);
